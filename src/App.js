733ef4d9172f0b8e9c81a9fd802699e541e96510
jaziel <pech2601@gmail.com>
2024-02-18 16:48:22 -0600

    solution

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter} from "react-router-dom";
+//import {BrowserRouter} from 'react-router-dom';
+import Rutas from './routes/Rutas';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div>
+      <Rutas/>
+    
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
